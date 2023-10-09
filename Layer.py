0a496e0e3b17f19377f4f44106b19e731037a714
Gustavo V. M. <gus_vila_mitra@proton.me>
2023-10-09 15:04:20 +0100

    Started backwards pass, TODO: fix error on Layer.py

--- Layer.py
+++ Layer.py
@@ -1,19 +1,37 @@
 import numpy as np 
-
-class Dense:
+import Activation 
+import Loss
+class Layer:
     def __init__(self, num_neurons, num_inputs):
         self.num_neurons = num_neurons
         self.num_inputs = num_inputs
         self.weights = 0.1 * np.random.randn(num_inputs, num_neurons)
         self.bias = np.zeros((1, num_neurons))
 
+
+
+
+class Hidden(Layer):
     def forward(self, input):
-        self.output = np.dot(input, self.weights) + self.bias
+        relu = Activation.ReLU
+        
+        self.pre_activation = np.dot(input, self.weights) + self.bias
+        self.output = relu().forward(self.pre_activation)
 
-    def backwards_output(self, dLoss, activated_outputs):
-        self.detaBias = [0]
-        self.delta = dLoss
+class Output(Layer):
+    def forward(self, input):
+        softmax = Activation.Softmax
 
-        self.deltaWeights = np.dot(activated_outputs.T, dLoss) / self.num_inputs 
+        self.pre_activation = np.dot(input, self.weights) + self.bias
+        self.output = softmax().forward(self.pre_activation)
+
+    def backwards(self, y_expect):
+
+        squaredError = Loss.SquaredError
+        
+        self.delta = squaredError.derivative(y_expect=y_expect, y_pred=self.output)
+
+
+
 
 
